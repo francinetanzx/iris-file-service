72278b969145426a15cacf210d059c1af5e50c18
eechin1997 <eechin.tay@outlook.com>
2023-05-04 15:52:00 +0800

    File Server and Service Physical Architecture
    This should work for file server -> file service architecture design

--- index.js
+++ index.js
@@ -1,95 +1,107 @@
-const fs = require("fs");
-const request = require("request");
 const express = require('express');
-const FormData = require('form-data');
-const axios = require('axios')
-const { Readable } = require('stream');
-const streamifier = require('streamifier');
-
-const fileUpload = require('express-fileupload');
 const app = express();
 const port = 3000;
 
+const fileUpload = require('express-fileupload');
 app.use(fileUpload());
+
+let Client = require('ssh2-sftp-client');
+const client = new Client();
+
+require('dotenv').config();
+
+app.get('/download/CSVtoJSON', function (req, res) {
+    res.sendFile(__dirname + "/file-download.html")
+});
 
 // acceptable organizations: sss, ace, fmmd 
 app.get('/upload/CSVtoJSON', function (req, res) {
     res.sendFile(__dirname + "/file-upload.html")
 });
 
-// app.post('/upload/:organization', async function (req, res) {
-//     const propertiesPath = "properties.json";
-//     let residingFilePath = "";
-//     if (fs.existsSync(propertiesPath)){
-//         const jsonString = fs.readFileSync(propertiesPath);
-//         residingFilePath = JSON.parse(jsonString).final_path + "/";
-//     }
-//     const filename = req.files.fileUpload.name
-//     if (!req.params)
-//         return res.send("NO PARAMS PASSED")
+async function uploadFile(localFile, remoteFile) {
+    console.log(`Uploading ${localFile} to ${remoteFile} ...`);
+    try {
+       client.put(localFile, remoteFile).then((data) => {
+        if (data == `Uploaded data stream to ${remoteFile}`) {
+            client.end()
+            console.log(`Uploaded file: ${remoteFile}`);
+        }
+       });
+    } catch (err) {
+      console.error('Uploading failed:', err);
+    }
+}
 
-//     if (!req.params.organization)
-//         return res.send("NO organization PASSED")
+async function connect(options) {
+    console.log(`Connecting to ${options.host}:${options.port}`);
+    try {
+      await client.connect(options);
+    } catch (err) {
+      console.log('Failed to connect:', err);
+    }
+}
 
-//     if (req.params.organization === "") {
-//         res.send("organization EMPTY.")
-//     } else {
-//         const formData  = new FormData();
-//         const uploadDirectory = "./" + req.params.organization + "/" + residingFilePath
-//         if (!fs.existsSync(uploadDirectory)){
-//             fs.mkdir(residingFilePath, { recursive: true }, (err) => {
-//                 if (err) throw err;
-//             });
-//         }
-//         filePath = uploadDirectory + filename
-//         console.log(filePath)
-//         formData.append('organization', req.params.organization);
-//         formData.append(filename, fs.createReadStream(filePath));
-//         const res = await axios.post("http://172.20.10.5:3000/upload", formData, {
-//             headers: formData.getHeaders()
-//         });
-//     }
-// });
-
-function bufferToStream(binary) {
-
-    const readableInstanceStream = new Readable({
-      read() {
-        this.push(binary);
-        this.push(null);
-      }
-    });
-
-    return readableInstanceStream;
+async function downloadFile(remoteFile) {
+    console.log(`Downloading ${remoteFile} to ${remoteFile} ...`);
+    try {
+      await client.get(remoteFile, remoteFile);
+    } catch (err) {
+      console.error('Downloading failed:', err);
+    }
 }
 
 app.post('/upload/:organization', async function (req, res) {
-    console.log(req.files)
-    const filename = req.files.fileUpload.name
+    const filename = req.params.organization + "/" + req.files.fileUpload.name
+    console.log("filename: ", filename)
     if (!req.params)
         return res.send("NO PARAMS PASSED")
 
     if (!req.params.organization)
-        return res.send("NO organization PASSED")
+        return res.send("NO ORGANIZATION PASSED")
 
     if (req.params.organization === "") {
-        res.send("Organization EMPTY.")
+        res.send("ORGANIZATION EMPTY.")
     } else {
-        const formData  = new FormData();
-        console.log(req.files.fileUpload.data)
-        formData.append('organization', req.params.organization);
-        // const stream = bufferToStream(req.files.fileUpload.data)
-        // const stream = Readable.from(req.files.fileUpload.data);
-        // console.log(stream)     
-        // var buffer = Buffer.from( new Uint8Array(req.files.fileUpload.data) );
-
-        formData.append(filename, streamifier.createReadStream(new Buffer (req.files.fileUpload.data)).pipe(process.stdout));
-        const res = await axios.post("http://172.20.10.5:3000/upload", formData, {
-            headers: formData.getHeaders()
-        });
+        const host = process.env.HOST
+        const port = process.env.PORT
+        const username = process.env.NAME
+        const password = process.env.PASSWORD
+        console.log(host)
+        console.log(port)
+        console.log(username)
+        console.log(password)
+        await connect({ host, port, username, password })
+        await uploadFile(req.files.fileUpload.data, filename)
+        res.send(`<p>http://172.20.10.11:3000/download/${filename}</p>`)
     }
 });
 
-app.listen(port, () => {
+app.post('/download', async function (req, res) {
+    const requestedFilePath = req.body["fileDownload"]
+    if (!req.body)
+        return res.send("NO PARAMS PASSED")
+    if (!requestedFilePath)
+        return res.send("NO filepath PASSED")
+    else {
+        const startIndex = requestedFilePath.indexOf("download") + 9
+        const filepath = requestedFilePath.substring(startIndex)
+        // const requestedFileName  = filepath.substring(filepath.indexOf('/')+1)
+        // const requestedOrganization  = filepath.substring(0,filepath.indexOf('/'))
+        const host = process.env.HOST
+        const port = process.env.PORT
+        const username = process.env.NAME
+        const password = process.env.PASSWORD
+        // const finalRequestedPath = requestedOrganization + "/"  + requestedFileName
+        // console.log("requestedOrganization: ", requestedOrganization)
+        // console.log("requestedFileName: ", requestedFileName)
+        // console.log("finalRequestedPath: ", finalRequestedPath)
+        await connect({ host, port, username, password });
+        await downloadFile(filepath)
+        res.download(`${__dirname}/${filepath}`)
+    }
+});
+
+app.listen(port, "172.20.10.11", () => {
     console.log(`Example app listening on port ${port}`)
 })
